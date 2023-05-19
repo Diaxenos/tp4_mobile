387644bb3235928b08b8286ac3b9bfd0c81eabb2
diaxenos <Paquette.alecc@gmail.com>
2023-05-19 11:38:38 -0400

    Boradcast remis

--- app/src/main/java/ca/cegepgarneau/tp4_mobile/MyBroadCastReceiver.kt
+++ app/src/main/java/ca/cegepgarneau/tp4_mobile/MyBroadCastReceiver.kt
@@ -2,14 +2,22 @@ package ca.cegepgarneau.tp4_mobile
 
 import android.content.Context
 import android.content.Intent
+import android.provider.Telephony
 import android.telephony.SmsMessage
 import android.util.Log
-
+import ca.cegepgarneau.tp4_mobile.model.Marker
 
 
 class MyBroadCastReceiver : android.content.BroadcastReceiver() {
 
+    private lateinit var marker: Marker
+
     override fun onReceive(context: Context, intent: Intent) {
-        val bundle = intent.extras
+        if (!intent?.action.equals(Telephony.Sms.Intents.SMS_RECEIVED_ACTION)) return
+        val extractMessages = Telephony.Sms.Intents.getMessagesFromIntent(intent)
+        extractMessages.forEach { smsMessage -> Log.v("TAG", smsMessage.displayMessageBody) }
+
+
+
     }
 }
